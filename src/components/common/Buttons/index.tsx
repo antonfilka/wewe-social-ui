9e00d2235733d7408f1385112cc77d0b8d3ef38d
PalinaHaidzina <palina.h@gofarsoft.org>
2024-10-08 19:47:50 +0300

    Добавлена пробная страница dashboard, на нее нанесены компоненты кнопки, рейтинга и строки для поиска

--- src/components/common/Buttons/index.tsx
+++ src/components/common/Buttons/index.tsx
@@ -1,9 +1,16 @@
 import { Button as ButtonAnt, ButtonProps as ButtonPropsAnt } from 'antd';
 
+type ButtonVariantType = 'error' | 'success';
+
 interface ButtonProps extends ButtonPropsAnt {
     children?: React.ReactNode;
+    buttonType?: ButtonVariantType;
 }
 
-export const Button = (props: ButtonProps) => {
-    return <ButtonAnt {...props}>{props.children}</ButtonAnt>;
+export const BaseButton = ({ children, buttonType, ...props }: ButtonProps) => {
+    return (
+        <ButtonAnt {...props} className={`base-button ${buttonType}`}>
+            {children}
+        </ButtonAnt>
+    );
 };
