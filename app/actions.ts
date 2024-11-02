c2f2b5ce95efa33a106a7266537266c2e39fcb32
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-11-02 19:32:18 +0300

    fix: build issue

--- app/actions.ts
+++ app/actions.ts
@@ -30,7 +30,7 @@ export async function sendNotification(message: string) {
 
     try {
         await webpush.sendNotification(
-            subscription,
+            JSON.parse(JSON.stringify(subscription)),
             JSON.stringify({
                 title: 'Test Notification',
                 body: message,
